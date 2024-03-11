bc3db6b7f8f4938fed972d09dbed68702ec0a77a
Caden-Hornyak <108236501+Caden-Hornyak@users.noreply.github.com>
2024-03-10 23:43:07 -0700

    further work on piano. Annoying bug fixes

--- frontend/src/components/Piano/Piano.js
+++ frontend/src/components/Piano/Piano.js
@@ -1,41 +1,121 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import PianoKey from './PianoKey'
 import { Howl } from 'howler'
 import './Piano.css'
 
-const Piano = () => {
+const Piano = ({ start=12, end=60 }) => {
+  
+  let notes = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
+
+  let keyboard_keys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 
+                      'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 
+                      'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'enter',
+                      'l_shft', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'r_shft']
+
+  let piano_keys_ref = useRef({})
+  let dic
+
+  let [recording, set_recording] = useState(false)
+  let [recorded_song, set_recorded_song] = useState([])
+  let piano_keys
+
+  let create_piano = () => {
+    let keyboard_it = 0
+    piano_keys = []
+    piano_keys_ref.current = {} 
+    dic = new Map()
+
     
-    let notes = ['A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab']
+    for (let i = start; i < end; i++) {
 
-    let create_piano = (start, end) => {
-        let piano_keys = []
+      if (keyboard_it >= keyboard_keys.length)
+        break
 
-        let add = 0;
-        for (let i = start; i < end; i++) {
-            if (i % 12 >= 3) {
-                add = 1
-            } else {
-                add = 0
-            }
-            console.log(notes[(i % 12)], add+Math.floor(i / 12), i)
-            piano_keys.push(<PianoKey note={notes[(i % 12)] + (add+Math.floor(i / 12)).toString()}
-            color={notes[(i % 12)].length == 1 ? 'white': 'black'} />)
-        }
-        return piano_keys
+      let note = notes[(i % 12)] + (1+Math.floor(i / 12)).toString()
+
+      piano_keys.push(<PianoKey note={note} color={notes[(i % 12)].length == 1 ? 'white': 'black'} 
+      keyboard_key={keyboard_keys[keyboard_it]}
+      key={note}
+      innerRef={ref => piano_keys_ref.current[i] = ref}
+      recorded_song={recorded_song}
+      set_recorded_song={set_recorded_song}
+      recording={recording}
+      />)
+
+      
+      dic.set(keyboard_keys[keyboard_it], i)
+      keyboard_it += 1
+
     }
 
+    return piano_keys
+  }
+  
+  let [test, set_test] = useState([])
+
+  let recording_action = (action) => {
+    if (action === 'start') {
+      set_recording(prev => !prev)
+      // some other stuff
+      console.log("recording")
+    } else if (action === 'end') {
+      console.log("stopped recording")
+      set_recording(false)
+
+      set_recorded_song([])
+    }
+  }
+
+  useEffect(() => {
+
+    const playPianoKey = (event) => {
+
+      let pressed_key = event.key.toLowerCase();
+
+      if (pressed_key === 'shift') {
+        pressed_key = event.location === 1 ? 'l_shft': 'r_shft'
+      }
+      const piano_key = dic.get(pressed_key);
+
+      if (piano_key) {
+        piano_keys_ref.current[piano_key].click()
+      }
+
+
+    };
+
+    document.addEventListener('keydown', playPianoKey);
+    create_piano()
+
+    set_test(create_piano())
+
+
+    return () => {
+      document.removeEventListener('keydown', playPianoKey);
+    };
+  }, [])
+
+  let clear_song = () => {
+    set_recorded_song([])
+    console.log("Song cleared")
+  }
+
+
   return (
-    <div id='piano'>
-        <div id="first-level" >
-            {create_piano(0, 28)}
-        </div>
-        <div id="second-level" >
-            {create_piano(28, 56)}
-        </div>
-        <div id="third-level" >
-            {create_piano(56, 84)}
-        </div>
-        
+    <div id='piano-wrapper'>
+      <div id='piano-buttons'>
+        <button id='recording-button' onClick={() => recording ? recording_action('end') : recording_action('start')} ></button>
+        <button id='clear-button' onClick={() => clear_song()} ></button>
+      </div>
+      <div id='piano'>
+          <div id="first-level" >
+            {test}
+          </div>
+          <div id="second-level" >
+              {/* {piano_keys && console.log(piano_keys.slice(Math.floor((piano_keys.length) / 2), piano_keys.length))} */}
+          </div>
+          
+      </div>
     </div>
   )
 }
--- frontend/src/components/Piano/PianoKey.js
+++ frontend/src/components/Piano/PianoKey.js
@@ -2,25 +2,48 @@ import React, { useEffect, useState } from 'react'
 import { Howl } from 'howler'
 import F3 from '../../audio/pianokeys/F3.mp3'
 
-const Pianokey = ({note, color}) => {
+const Pianokey = ({note, color, keyboard_key, innerRef, recorded_song, set_recorded_song, recording}) => {
+  console.log("RERENDER")
+  const [audio, setAudio] = useState(null);
+  const timeThreshold = 50;
 
-    const [audio, setAudio] = useState(null);
+  let load_note = async () => {
+    const note_src = await import(`../../audio/pianokeys/${note}.mp3`);
+    setAudio(new Howl({ src: [note_src.default]}))
+  }
+  
+  let playSound = () => {
 
-    let load_note = async () => {
-        const note_src = await import(`../../audio/pianokeys/${note}.mp3`);
-        setAudio(new Howl({ src: [note_src.default]}))
+    // record pressed notes in linear time array
+    console.log(recording)
+    if (recording) {
+      
+      set_recorded_song((prev_song) => {
+        const timestamp = new Date().getTime();
+
+        let l = [...prev_song];
+
+        if (l.length > 0 && 
+          timestamp - l[l.length-1][1][0] < timeThreshold) {
+            l[l.length-1][0].push(note)
+            l[l.length-1][1] = [timestamp]
+            return l
+          } else {
+            console.log(l)
+            l.push([[note], [timestamp]])
+            return l
+          }
+      })
     }
-    
-    let playSound = () => {
-        audio.play()
-    }
+    audio.play()
+  }
 
-    useEffect(() => {
-        load_note()
-    }, [])
+  useEffect(() => {
+    load_note()
+  }, [])
 
   return (
-    <div className={color} onClick={() => playSound()} >{note}</div>
+    <div ref={innerRef} className={color} onClick={() => playSound()} >{keyboard_key}</div>
   )
 }
 
